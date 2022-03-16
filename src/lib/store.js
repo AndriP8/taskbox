77ff540ecb83cf5148b97a8c644d746c7e7159ae
AndriP8 <andrikcoc26@gmail.com>
2022-03-16 18:51:39 +0700

    chore: added fetching in redux

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,8 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createSlice,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
 
 const defaultTasks = [
   { id: "1", title: "Something", state: "TASK_INBOX" },
@@ -13,6 +17,19 @@ const taskBoxData = {
   error: null,
 };
 
+export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/?userId=1"
+  );
+  const data = await response.json();
+  const result = data.map((task) => ({
+    id: task.id,
+    title: task.title,
+    state: task.completed ? "TASK_ARCHIVED" : "TASK_INBOX",
+  }));
+  return result;
+});
+
 const TasksSlice = createSlice({
   name: "taskbox",
   initialState: taskBoxData,
@@ -22,6 +39,24 @@ const TasksSlice = createSlice({
       const task = state.tasks.findIndex((task) => task.id === id);
       if (task >= 0) state.tasks[task].state = newTaskState;
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = null;
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.status = "failed";
+        state.error = "Something went wrong";
+        state.tasks = [];
+      });
   },
 });
 
