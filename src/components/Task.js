d71c5e9844cd66f5cfca020559c68ab2ee6fbc13
AndriP8 <andrikcoc26@gmail.com>
2022-03-16 16:00:08 +0700

    chore: added initial task and stories

---
+++ src/components/Task.js
@@ -0,0 +1,58 @@
+import PropTypes from "prop-types";
+
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label htmlFor="checked" className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === "TASK_ARCHIVED"}
+          disabled={true}
+          name="checked"
+        />
+        <span
+          className="checkbox-custom"
+          onClick={() => onArchiveTask(id)}
+          aria-label={`archiveTask-${id}`}
+        />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input Title"
+        />
+      </div>
+      <div
+        className="actions"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {state !== "TASK_ARCHIVED" && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span
+              className="icon-star"
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            ></span>
+          </a>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Task;
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
