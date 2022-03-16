e4214c98c43709ad495674bc2614c186420bd7cc
AndriP8 <andrikcoc26@gmail.com>
2022-03-16 08:45:53 +0700

    chore: initial setup

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ["../src/components/**/*.stories.js"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack4",
+  },
+};
